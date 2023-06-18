9e2e5a275bc7093b495ccc74dbc1dba056ad4988
nut077 <nuttapon.tavee@gmail.com>
2023-06-18 11:57:21 +0700

    Initial commit

---
+++ src/test/java/com/github/nut077/libraryeventsconsumer/LibraryEventsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nut077.libraryeventsconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
